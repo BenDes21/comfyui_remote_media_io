78680ecfb1392814306f6c70fc7e85be2ab752ee
BenDes21 <153294763+BenDes21@users.noreply.github.com>
2025-09-19 10:24:10 +0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,8 @@
 # /comfyui/custom_nodes/comfyui_remote_media_io/__init__.py
+from .src.comfyui_remote_media_io.nodes import NODE_CLASS_MAPPINGS
+from .src.comfyui_remote_media_io.nodes import NODE_DISPLAY_NAME_MAPPINGS
 
 __all__ = [
     "NODE_CLASS_MAPPINGS",
     "NODE_DISPLAY_NAME_MAPPINGS",
 ]
-
-# Cette ligne importe TOUT ce qui est défini dans saver_nodes.py
-from .src.comfyui_remote_media_io.saver_nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
-
-print("Custom Nodes: BunnyCDN Uploader (remplaçant remote_media_io) chargé.")
